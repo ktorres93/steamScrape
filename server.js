1717ff5fdd87b45c2216ee9730bcf096c6cfa68a
Kevin <ktorres93@knights.ucf.edu>
2017-07-19 22:17:54 -0400

    added node packages and server.

---
+++ server.js
@@ -0,0 +1,20 @@
+/**
+ * Created by kevin on 7/19/2017.
+ */
+const cheerio = require('cheerio');
+const express = require('express');
+const request = require('request');
+const mongojs = require('mongojs');
+
+$ = require('cheerio');
+
+var url = 'http://store.steampowered.com/';
+
+request(url, function (error, response, body){
+    console.log(error,response,body);
+    console.log('statusCode:',response && response.statusCode);
+    console.log("body", body);
+
+    var $ =cheerio.load(body);
+    console.log($('.tab_item_name text'));
+});
